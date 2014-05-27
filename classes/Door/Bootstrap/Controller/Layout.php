b8db7b57ccc3205dc1832743fbf6fa44ffebaa9f
root <root@serginhoPC.Dlink>
2014-05-27 15:14:42 +0400

    init

---
+++ classes/Door/Bootstrap/Controller/Layout.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Created by Sachik Sergey
+ * box@serginho.ru
+ */
+namespace Door\Bootstrap\Controller;
+/**
+ * Description of Layout
+ *
+ * @author serginho
+ */
+class Layout extends \Door\Core\Controller\Layout {
+	
+	public function init() {
+		
+		
+		
+		parent::init();
+	}
+	
+}
